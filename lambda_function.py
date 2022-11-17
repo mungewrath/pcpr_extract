610dd26ccc123a8ead8e6fec40e869b0c7aa81df
Matthew Unrath <matthew.unrath@parivedasolutions.com>
2022-11-17 10:22:57 -0800

    Clean up extra code

--- lambda_function.py
+++ lambda_function.py
@@ -8,8 +8,6 @@ def lambda_handler(event, context):
     content = {}
 
     s3 = boto3.client('s3')
-    # with open("tmp/{}".format(filename), "rb") as f:
-        # s3.upload_fileobj(f, "mungewrath-pcpr-poc", "output/pcpr/{}".format(filename))
     
     textract_data_stream = io.BytesIO()
     s3.download_fileobj(event['Bucket'], event['TextractResults'], textract_data_stream)
@@ -20,14 +18,6 @@ def lambda_handler(event, context):
     s3.download_fileobj(event['Bucket'], event['ComprehendResults'], comprehend_data_stream)
     comprehend_data_stream.seek(0)
     content['comprehendMedicalEntities'] = json.load(comprehend_data_stream)
-
-    # s3.download_file(event['Bucket'],event['TextractResults'],'tmp/TextractResults.json')
-    # s3.download_file(event['Bucket'],event['ComprehendResults'],'tmp/ComprehendResults.json')
-    
-    # with open("tmp/TextractResults.json", "r") as f:
-        # content['textractAnalysis'] = json.load(f)
-    # with open("tmp/ComprehendResults.json", "r") as f:
-        # content['comprehendMedicalEntities'] = json.load(f)
 
     print("Textract:")
     print(content['textractAnalysis'])
